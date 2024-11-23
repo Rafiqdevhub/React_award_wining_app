b0e6131b10831196cad928b88118614fa3c64a35
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-11-23 19:50:49 +0500

    modify

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -140,7 +140,7 @@ const Hero = () => {
         </div>
 
         <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
-          Programming
+          Gaming{" "}
         </h1>
 
         <div className="absolute left-0 top-0 z-40 size-full">
@@ -165,7 +165,7 @@ const Hero = () => {
       </div>
 
       <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
-        programming
+        Gaming{" "}
       </h1>
     </div>
   );

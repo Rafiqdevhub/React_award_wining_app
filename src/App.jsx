c9f3db7126afa1b8d53ab38096e8e62a13838936
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-11-23 17:35:51 +0500

    set Navbar with install necessary dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
-    </div>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
+    </main>
   );
 };
 
